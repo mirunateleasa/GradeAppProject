2ec19191379bd7734173a2d98752d8c48fa8e645
mirunateleasa <teleasamiruna19@stud.ase.ro>
2021-12-28 12:21:41 +0200

    added the beginning of gradeProject

---
+++ grade-frontend/src/components/GradeProject/Grader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Grader = () => {
+    return (
+        <div className='containerGrader'>
+            <iframe src=''></iframe> {/* aici sa adaugam pdf-ul proiectului si butoane pentru note. Inca nu avem proiectele inserate*/}
+        </div>
+    )
+}
+
+export default NavBar
